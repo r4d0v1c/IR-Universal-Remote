a0527e182a8e2d8ecb0792139032884a01763e57
r4d0v1c <radovic.ra47.2022@uns.ac.rs>
2025-10-11 16:45:28 +0200

    Uspesno implementirana konekcija sa ESP32 putem dvostruke provere, imena i MAC adreese.

---
+++ LPRS2/app/src/main/java/com/example/universal_ac_tv_remote/utils/Constants.java
@@ -0,0 +1,10 @@
+package com.example.universal_ac_tv_remote.utils;
+
+import java.util.UUID;
+
+public class Constants {
+    public static final int REQUEST_BLUETOOTH_PERMISSIONS = 1;
+    public static final int REQUEST_ENABLE_BT = 2;
+    public static final UUID ESP32_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    public static final String ESP32_MAC_ADDRESS = "B0:B2:1C:44:C0:CE";
+}
